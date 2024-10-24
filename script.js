3a05f054e4eb72cf3402c47457ac806aa1bddc68
Ayad Almaliki <126099043+Ayadhost@users.noreply.github.com>
2024-10-24 19:52:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // أوقات الصلاة
+    // أوقات الصلاة بدون ثواني
     const prayerTimes = {
         fajr: "4:51",
         dhuhr: "11:48",
         maghrib: "5:18"
     };
 
-    // ضبط الأوقات في العناصر
+    // تعيين أوقات الصلاة في الجدول
     document.getElementById("fajr-time").textContent = prayerTimes.fajr;
     document.getElementById("dhuhr-time").textContent = prayerTimes.dhuhr;
     document.getElementById("maghrib-time").textContent = prayerTimes.maghrib;
