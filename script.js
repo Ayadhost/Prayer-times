9ad5e2534a881594c735f1522a3de0ee7bfafea6
Ayad Almaliki <126099043+Ayadhost@users.noreply.github.com>
2024-10-24 20:13:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("fajr-time").textContent = prayerTimes.fajr;
     document.getElementById("dhuhr-time").textContent = prayerTimes.dhuhr;
     document.getElementById("maghrib-time").textContent = prayerTimes.maghrib;
+
+    // تحديث التاريخ والوقت الحالي في الحقل تحت العنوان
+    function updateDateTime() {
+        const currentDateTime = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+        const formattedDateTime = currentDateTime.toLocaleDateString('ar-EG', options);
+        document.getElementById("current-date-time").textContent = formattedDateTime;
+    }
+
+    // استدعاء الدالة عند التحميل وتحديث الوقت كل ثانية
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
 });
