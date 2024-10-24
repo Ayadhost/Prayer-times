19fd11b9fab632806843b7750b11f264932834f4
Ayad Almaliki <126099043+Ayadhost@users.noreply.github.com>
2024-10-25 01:57:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // إعدادات حساب الصلاة مع الزوايا
     const params = adhan.CalculationMethod.Other(); 
     params.fajrAngle = 18; // زاوية الفجر للمذهب الجعفري
-    params.ishaAngle = 14; // زاوية العشاء للمذهب الجعفري
+    params.ishaAngle = 15; // زاوية العشاء للمذهب الجعفري
 
     const date = new Date();
     const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
